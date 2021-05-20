20c7947032c69483fd310c32ba688b5ae5697124
Christian cucco <cdc-96@hotmail.com>
2021-05-20 19:59:01 -0300

    add common functions

--- src/lib/commonConnection/commonConnection.py
+++ src/lib/commonConnection/commonConnection.py
@@ -16,3 +16,21 @@ class CommonConnection:
         except socket.error:
             return
         return
+
+    def sendEndFile(socket, host, port, filename, lastBytesReceived):
+        addr = (host, port)
+        message = 'E'+filename
+        try:
+            socket.sendto(message.encode(), addr)
+        except socket.error:
+            return
+        return
+
+    def sendMessage(socket, host, port, filename, message, lastBytesReceived):
+        addr = (host, port)
+        data = 'T'+filename+";"+str(lastBytesReceived)+";"+message
+        try:
+            socket.sendto(data.encode(), addr)
+        except socket.error:
+            return
+        return
